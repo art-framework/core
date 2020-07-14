32942c52e2a5e614c99b66d95067969e67469b4b
Michael Reichenbach <michael.reichenbach@interhyp.de>
2020-07-14 10:20:45 +0200

    Revert "try out some kotlin"
    This reverts commit 662f920f

--- art-api/src/main/java/net/silthus/art/impl/DefaultConfiguration.kt
+++ art-api/src/main/java/net/silthus/art/impl/DefaultConfiguration.kt
@@ -1,100 +1,89 @@
-/*
- * Copyright 2020 ART-Framework Contributors (https://github.com/Silthus/art-framework)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package net.silthus.art.impl;
 
-package net.silthus.art.impl
+import lombok.NonNull;
+import net.silthus.art.*;
+import net.silthus.art.conf.Settings;
 
-import net.silthus.art.*
-import net.silthus.art.conf.Settings
+import java.util.Optional;
 
-class DefaultConfiguration internal constructor(art: ArtProvider, scheduler: Scheduler?, storage: Storage, settings: Settings, targets: TargetProvider) : Configuration {
+public class DefaultConfiguration implements Configuration {
 
-    private var settings: Settings
+    /**
+     * Serial version UID
+     */
+    private static final long serialVersionUID = 4296025820492453890L;
 
-    @Transient
-    private var art: ArtProvider
+    private Settings settings;
 
-    @Transient
-    private var scheduler: Scheduler? = null
+    private transient ArtProvider art;
+    private transient Scheduler scheduler;
+    private transient Storage storage;
+    private transient TargetProvider targets;
 
-    @Transient
-    private var storage: Storage
-
-    @Transient
-    private var targets: TargetProvider
-
-    init {
-        this.art = art
-        this.scheduler = scheduler
-        this.storage = storage
-        this.settings = settings
-        this.targets = targets
+    public DefaultConfiguration() {
+        this(null, null, Storage.DEFAULT, Settings.DEFAULT, )
     }
 
-    constructor() : this(TODO(), TODO(), Storage.DEFAULT, Settings.DEFAULT, TODO())
-
-    override fun art(): ArtProvider {
-        return art
+    DefaultConfiguration(ArtProvider art, Scheduler scheduler, Storage storage, Settings settings, TargetProvider targets) {
+        set(art);
+        set(scheduler);
+        set(storage);
+        set(settings);
+        set(targets);
     }
 
-    override fun scheduler(): Scheduler? {
-        return scheduler
+    @Override
+    public ArtProvider art() {
+        return art;
     }
 
-    override fun storage(): Storage {
-        return storage
+    @Override
+    public Optional<Scheduler> scheduler() {
+        return Optional.ofNullable(scheduler);
     }
 
-    override fun settings(): Settings {
-        return settings
+    @Override
+    public Storage storage() {
+        return storage;
     }
 
-    override fun targets(): TargetProvider {
-        return targets
+    @Override
+    public Settings settings() {
+        return settings;
     }
 
-    override fun set(artProvider: ArtProvider): Configuration {
-        art = artProvider
-        return this
+    @Override
+    public TargetProvider targets() {
+        return targets;
     }
 
-    override fun set(scheduler: Scheduler?): Configuration {
-        this.scheduler = scheduler
-        return this
+    @Override
+    public Configuration set(@NonNull ArtProvider artProvider) {
+        this.art = artProvider;
+        return this;
     }
 
-    override fun set(storage: Storage): Configuration {
-        this.storage = storage
-        return this
+    @Override
+    public Configuration set(Scheduler scheduler) {
+        this.scheduler = scheduler;
+        return this;
     }
 
-    override fun set(settings: Settings): Configuration {
-        this.settings = settings
-        return this
+    @Override
+    public Configuration set(@NonNull Storage storage) {
+        this.storage = storage;
+        return this;
     }
 
-    override fun set(targetProvider: TargetProvider): Configuration {
-        targets = targetProvider
-        return this
+    @Override
+    public Configuration set(@NonNull Settings settings) {
+        this.settings = settings;
+        return this;
     }
 
-    companion object {
-        /**
-         * Serial version UID
-         */
-        private const val serialVersionUID = 4296025820492453890L
-
+    @Override
+    public Configuration set(@NonNull TargetProvider targetProvider) {
+        this.targets = targetProvider;
+        return this;
     }
 }
